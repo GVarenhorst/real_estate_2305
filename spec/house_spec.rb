9c499204fd1c048b4f62d60e7beb2af91c73ae08
Garrett Varenhorst <garrettvarenhorst@gmail.com>
2023-05-18 12:06:32 -0500

    test: Add 'categorize rooms' test

--- spec/house_spec.rb
+++ spec/house_spec.rb
@@ -41,5 +41,24 @@ RSpec.describe House do
       expect(house.above_market_average?).to eq(false)
 
     end
+
+    it "can categorize rooms" do
+
+      house = House.new("$400000", "123 sugar lane")
+
+      room_1 = Room.new(:bedroom, 10, '13')
+      room_2 = Room.new(:bedroom, 11, '15')
+      room_3 = Room.new(:living_room, 25, '15')
+      room_4 = Room.new(:basement, 30, '41')
+
+      house.add_room(room_1)
+      house.add_room(room_2)
+      house.add_room(room_3)
+      house.add_room(room_4)
+
+      expect(house.rooms_from_category(:bedroom)).to eq([room_1, room_2])
+      expect(house.rooms_from_category(:living_room)).to eq([room_3])
+      expect(house.rooms_from_category(:basement)).to eq([room_4])
+    end
   end
 end
