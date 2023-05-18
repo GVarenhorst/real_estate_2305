57403eb3b7795eba9db1632a5a3c371a320e8639
Garrett Varenhorst <garrettvarenhorst@gmail.com>
2023-05-18 10:56:55 -0500

    Add requires for room.rb and house.rb, create tests for Iteration 2, and test: pass exists test

---
+++ spec/house_spec.rb
@@ -0,0 +1,33 @@
+require './lib/room'
+require './lib/house'
+
+RSpec.describe House do
+  describe "Iteration 2" do
+    it "exists" do
+
+      house = House.new("$400000", "123 sugar lane")
+
+      expect(house).to be_a(House)
+
+    end
+
+    xit "has attributes" do
+
+      house = House.new("$400000", "123 sugar lane")
+
+      expect(house.price).to eq(400000)
+      expect(house.address).to eq("123 sugar lane")
+      expect(house.rooms).to eq([])
+    end
+
+    xit "can add rooms" do
+
+      house = House.new("$400000", "123 sugar lane")
+
+      house.add_room(room_1)
+      house.add_room(room_2)
+
+      expect(house.rooms).to eq([room_1, room_2])
+    end
+  end
+end
